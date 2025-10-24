34fff29b5fd009518da4541e67381503afaefb4c
mohd-adnaan <iamadnaanmohd@gmail.com>
2025-10-24 14:40:00 -0400

    Initial Commit

---
+++ GloveGuide/GloveGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  GloveGuideApp.swift
+//  GloveGuide
+//
+//  Created by Mohammad Adnaan on 2025-10-24.
+//
+
+import SwiftUI
+
+@main
+struct GloveGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
